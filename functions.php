f58b4633f1970302378a00a58143f037aae94068
Jose M. Coronado <jcoronadoar@gmail.com>
2023-06-07 13:26:42 +0200

    08. Desarrollo y maquetación del header. Introducción de una pequeña animación desde un menú hamburguesa como opcuión propia al diseño
    Incluimos varias funciones en el functions.php:
    
    - Inclusión de css y js compilado y bootstrap en el header y footer
    - Creación de menu-main para la introducción de un menú principal en la página
    - Mostrar Feture images en las páginas
    - Importar plantilla para la creación posterior del CPT Perros desde carpeta inc

--- functions.php
+++ functions.php
@@ -8,146 +8,50 @@
  */
 
 if ( ! defined( '_S_VERSION' ) ) {
-	// Replace the version number of the theme on each release.
 	define( '_S_VERSION', '1.0.0' );
 }
 
-/**
- * Sets up theme defaults and registers support for various WordPress features.
- *
- * Note that this function is hooked into the after_setup_theme hook, which
- * runs before the init hook. The init hook is too late for some features, such
- * as indicating support for post thumbnails.
- */
-function wearecactus_setup() {
-	/*
-		* Make theme available for translation.
-		* Translations can be filed in the /languages/ directory.
-		* If you're building a theme based on wearecactus, use a find and replace
-		* to change 'wearecactus' to the name of your theme in all the template files.
-		*/
-	load_theme_textdomain( 'wearecactus', get_template_directory() . '/languages' );
-
-	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
-
-	/*
-		* Let WordPress manage the document title.
-		* By adding theme support, we declare that this theme does not use a
-		* hard-coded <title> tag in the document head, and expect WordPress to
-		* provide it for us.
-		*/
-	add_theme_support( 'title-tag' );
-
-	/*
-		* Enable support for Post Thumbnails on posts and pages.
-		*
-		* @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		*/
-	add_theme_support( 'post-thumbnails' );
-
-	// This theme uses wp_nav_menu() in one location.
-	register_nav_menus(
-		array(
-			'menu-1' => esc_html__( 'Primary', 'wearecactus' ),
-		)
-	);
-
-	/*
-		* Switch default core markup for search form, comment form, and comments
-		* to output valid HTML5.
-		*/
-	add_theme_support(
-		'html5',
-		array(
-			'search-form',
-			'comment-form',
-			'comment-list',
-			'gallery',
-			'caption',
-			'style',
-			'script',
-		)
-	);
-
-	// Set up the WordPress core custom background feature.
-	add_theme_support(
-		'custom-background',
-		apply_filters(
-			'wearecactus_custom_background_args',
-			array(
-				'default-color' => 'ffffff',
-				'default-image' => '',
-			)
-		)
-	);
-
-	// Add theme support for selective refresh for widgets.
-	add_theme_support( 'customize-selective-refresh-widgets' );
-
-	/**
-	 * Add support for core custom logo.
-	 *
-	 * @link https://codex.wordpress.org/Theme_Logo
-	 */
-	add_theme_support(
-		'custom-logo',
-		array(
-			'height'      => 250,
-			'width'       => 250,
-			'flex-width'  => true,
-			'flex-height' => true,
-		)
-	);
-}
-add_action( 'after_setup_theme', 'wearecactus_setup' );
-
-/**
- * Set the content width in pixels, based on the theme's design and stylesheet.
- *
- * Priority 0 to make it available to lower priority callbacks.
- *
- * @global int $content_width
- */
-function wearecactus_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'wearecactus_content_width', 640 );
-}
-add_action( 'after_setup_theme', 'wearecactus_content_width', 0 );
-
-/**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
-function wearecactus_widgets_init() {
-	register_sidebar(
-		array(
-			'name'          => esc_html__( 'Sidebar', 'wearecactus' ),
-			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'wearecactus' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		)
-	);
-}
-add_action( 'widgets_init', 'wearecactus_widgets_init' );
-
-/**
- * Enqueue scripts and styles.
- */
+/* Scripts WeAreCactus
+--------------------------------------------- */
 function wearecactus_scripts() {
-	wp_enqueue_style( 'wearecactus-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'wearecactus-style', 'rtl', 'replace' );
+	/* css */
+	wp_enqueue_style( 'wearecactus-base', get_stylesheet_uri(), array(), _S_VERSION );
+    wp_enqueue_style( 'bootstrap-style', get_template_directory_uri() . '/css/bootstrap.min.css', array(), null  );
+    wp_enqueue_style( 'wearecactus-style', get_template_directory_uri() . '/css/main.min.css', array(), wp_get_theme()->get( '1.0' )  );
 
+	/* js */
 	wp_enqueue_script( 'wearecactus-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+    wp_enqueue_script( 'bootstrap-scripts', get_template_directory_uri() . '/js/bootstrap.min.js', array(), false, true );
+    wp_enqueue_script( 'wearecactus-scripts', get_template_directory_uri() . '/js/main.min.js', array(), false, true );
 }
 add_action( 'wp_enqueue_scripts', 'wearecactus_scripts' );
+
+/* Menús WeAreCactus
+--------------------------------------------- */
+function wpb_custom_new_menu() {
+	register_nav_menus(
+	  array(
+		'main-menu' => __( 'Menú Principal' )
+	  )
+	);
+  }
+add_action( 'init', 'wpb_custom_new_menu' );
+
+function add_additional_class_on_li($classes, $item, $args) {
+    if(isset($args->add_li_class)) {
+        $classes[] = $args->add_li_class;
+    }
+    return $classes;
+}
+add_filter('nav_menu_css_class', 'add_additional_class_on_li', 1, 3);
+
+/* Feature Images WeAreCactus
+--------------------------------------------- */
+add_theme_support( 'post-thumbnails' );
+
+/* CPT Perros WeAreCactus
+--------------------------------------------- */
+include_once( get_stylesheet_directory() .'/inc/post-types/perros');
 
 /**
  * Implement the Custom Header feature.
@@ -175,9 +79,3 @@ require get_template_directory() . '/inc/customizer.php';
 if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
-
-function theme_scripts() {
-    wp_enqueue_style( 'theme-style', get_template_directory_uri() . '/css/main.min.css', array(), wp_get_theme()->get( '1.0' )  );
-    wp_enqueue_script( 'theme-scripts', get_template_directory_uri() . '/js/main.min.js', array(), false, true );
-}
-add_action( 'wp_enqueue_scripts', 'theme_scripts' );
--- header.php
+++ header.php
@@ -13,47 +13,52 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
+	<title><?php wp_title(); ?></title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&family=Poppins:wght@100;200;300;400;500;600;700;800&display=swap" rel="stylesheet">
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
+
 <?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'wearecactus' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$wearecactus_description = get_bloginfo( 'description', 'display' );
-			if ( $wearecactus_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $wearecactus_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+<div id="app" class="site-app">
+	<header id="header-cactus">
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'wearecactus' ); ?></button>
+		<div class="logo__header-cactus">
+			<a href="<?php echo get_home_url(); ?>">
+			<img src="/wearecactus/wp-content/uploads/2023/06/logo.png" alt="We Are Cactus" class="logo-dark">
+			<img src="/wearecactus/wp-content/uploads/2023/06/logo-W.png" alt="We Are Cactus" class="logo-light">
+		</a>
+		</div>
+
+		<div class="menu-icon">
+			<span class="menu-icon__line menu-icon__line-left"></span>
+			<span class="menu-icon__line"></span>
+			<span class="menu-icon__line menu-icon__line-right"></span>
+		</div>
+
+		<nav class="nav">
 			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+				wp_nav_menu(
+					array(
+						'theme_location' 	=> 'main-menu',
+						'menu_id'        	=> 'main-menu',
+						'menu_class' 	 	=> 'nav__list',
+						'container_class' 	=> 'nav__content',
+						'add_li_class'  	=> 'nav__list-item'
+					)
+				);
+				?>
+		</nav>
+
+	</header>
+
+	<main id="content-app">
